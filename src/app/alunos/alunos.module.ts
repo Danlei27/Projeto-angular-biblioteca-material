e30ffca141beb334883eb2599b77615ced79e97b
danlei27 <danleisantos@hotmail.com>
2020-05-08 23:37:19 -0300

    desenvolvendo as telas

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -5,14 +5,31 @@ import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
     imports: [
         CommonModule,
-        AlunosRoutingModule
+        AlunosRoutingModule,
+        MatButtonModule, 
+        MatListModule,
+        MatDividerModule,
+        MatGridListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule
+
     ],
     exports: [],
     declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
-    providers: [],
+    providers: [AlunosService],
 })
 export class AlunosModule { }
