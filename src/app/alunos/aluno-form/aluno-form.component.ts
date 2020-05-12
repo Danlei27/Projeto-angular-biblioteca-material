024d9843fa19d01a889d071395bd5ffbaeafa469
danlei27 <danleisantos@hotmail.com>
2020-05-12 20:24:09 -0300

    candeactivate de forma genérica com interface

--- src/app/alunos/aluno-form/aluno-form.component.ts
+++ src/app/alunos/aluno-form/aluno-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlunosService } from '../alunos.service';
 import { Subscription } from 'rxjs';
+import { IFormCanDeactivate } from 'src/app/guards/iform-candeactivate';
 
 @Component({
   selector: 'app-aluno-form',
@@ -9,7 +10,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./aluno-form.component.css']
 })
 
-export class AlunoFormComponent implements OnInit {
+export class AlunoFormComponent implements OnInit, IFormCanDeactivate {
 
   aluno: any;
   inscricao: Subscription;
@@ -49,4 +50,7 @@ export class AlunoFormComponent implements OnInit {
     return true;
   }
 
+  podeDesativar(){
+    return this.podeMudarRota();
+  }
 }
--- src/app/guards/alunos-deactivate.guard.ts
+++ src/app/guards/alunos-deactivate.guard.ts
@@ -2,15 +2,17 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AlunoFormComponent } from '../alunos/aluno-form/aluno-form.component';
+import { IFormCanDeactivate } from './iform-candeactivate';
 
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent>{
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate>{
     canDeactivate(
-        component: AlunoFormComponent,
+        component: IFormCanDeactivate,
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot,
         ): Observable<boolean>|Promise<boolean>|boolean {
-            return component.podeMudarRota? component.podeMudarRota():true;
+            // return component.podeMudarRota? component.podeMudarRota():true;
+            return component.podeDesativar();
     }
 }
