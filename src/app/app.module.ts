47b71cd497f8053c49cadd44d4772f28405ef52e
danlei27 <danleisantos@hotmail.com>
2020-05-13 00:03:10 -0300

    Definindo rota padrão e wildcard(rota não encontrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { AlunosGuard } from './guards/alunos.guard';
     AppComponent,
     LoginComponent,
     HomeComponent,
+    PaginaNaoEncontradaComponent,
     // CursosComponent,
     // CursoDetalheComponent,
     // CursoNaoEncontradoComponent
